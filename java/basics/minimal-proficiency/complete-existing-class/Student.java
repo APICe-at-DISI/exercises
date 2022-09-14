5607296cae922d2a25cd9f16f5b7ad316b0ff178
Danilo Pianini <danilo.pianini@unibo.it>
2022-09-14 18:54:53 +0200

    create exercise for complete-existing-class

--- java/basics/minimal-proficiency/complete-existing-class/Student.java
+++ java/basics/minimal-proficiency/complete-existing-class/Student.java
@@ -1,22 +1,16 @@
 class Student {
 
     // È buona pratica mettere i campi in testa alla classe
-    int id;
-    String name;
-    String surname;
-    int matriculationYear;
 
-    void build(String name, String surname, int id, int matriculationYear) {
-        this.name = name;
-        this.surname = surname;
-        this.id = id;
-        this.matriculationYear = matriculationYear;
+    void build(/* Qui vanno i parametri necessari a inizializzare l'oggetto */) {
+        /*
+         * Completare il corpo del metodo
+         */
     }
 
     void printStudentInfo() {
-        System.out.println("Student name: " + name);
-        System.out.println("Student surname: " + surname);
-        System.out.println("Student id: " + id);
-        System.out.println("Student matriculationYear: " + matriculationYear);
+        /*
+         * Completare il corpo del metodo
+         */
     }
 }
