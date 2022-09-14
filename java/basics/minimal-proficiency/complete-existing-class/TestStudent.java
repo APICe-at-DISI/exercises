5503a0cd80847098be2c1ab40faac7199e4e8b08
Danilo Pianini <danilo.pianini@unibo.it>
2022-09-14 18:57:06 +0200

    fix solution of complete-existing-class

--- java/basics/minimal-proficiency/complete-existing-class/TestStudent.java
+++ java/basics/minimal-proficiency/complete-existing-class/TestStudent.java
@@ -2,15 +2,11 @@ class TestStudent {
 
     public static void main(String[] args) {
         Student mario = new Student();
-        mario.build("Mario", "Rossi", 1014, 2019);
+        mario.build("Alex", "Balducci", 1015, 2019);
         mario.printStudentInfo();
 
-        Student luigi = new Student();
-        luigi.build("Luigi", "Gentile", 1015, 2019);
-        luigi.printStudentInfo();
-
         Student simone = new Student();
-        simone.build("Simone", "Bianchi", 1016, 2018);
+        simone.build("Angel", "Bianchi", 1016, 2018);
         simone.printStudentInfo();
 
         Student andrea = new Student();
