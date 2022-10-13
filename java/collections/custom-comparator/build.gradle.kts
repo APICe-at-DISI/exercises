dafb8f07e86236919f6b63af4a7903d937769075
Danilo Pianini <danilo.pianini@unibo.it>
2022-10-13 16:09:57 +0200

    prepare the actual exercise on comparators

--- java/collections/custom-comparator/build.gradle.kts
+++ java/collections/custom-comparator/build.gradle.kts
@@ -1,19 +1,3 @@
-import org.gradle.kotlin.dsl.registering
-
 plugins {
-    application
     java
-    id("org.danilopianini.gradle-java-qa") version "0.37.0"
-}
-
-repositories {
-    mavenCentral()
-}
-
-application {
-    mainClass.set("it.unibo.collections.comparators.UseSetWithOrder")
-}
-
-spotbugs {
-    omitVisitors.set(listOf("ComparatorIdiom"))
 }
