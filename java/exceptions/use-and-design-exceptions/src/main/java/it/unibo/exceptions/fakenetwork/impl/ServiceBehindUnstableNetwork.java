3c93698fea9979c93b278aa91fce3ad684c017ce
Danilo Pianini <danilo.pianini@unibo.it>
2022-10-19 18:54:55 +0200

    prepare the exception excercise playground

--- java/exceptions/use-and-design-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ java/exceptions/use-and-design-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,9 +29,6 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
-        if (failProbability < 0 || failProbability >= 1) {
-            throw new IllegalArgumentException("Probability must be in [0, 1[, provided: " + failProbability);
-        }
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
