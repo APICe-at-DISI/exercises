d4089ebdbff93be0ef845c5d4be64a8c5d14b1f8
Danilo Pianini <danilo.pianini@gmail.com>
2022-10-19 21:33:10 +0200

    prepare the exercise on implementing classes with maps

--- java/collections/implement-classes-with-maps/build.gradle.kts
+++ java/collections/implement-classes-with-maps/build.gradle.kts
@@ -1,15 +1,3 @@
-import org.gradle.kotlin.dsl.registering
-
 plugins {
-    application
     java
-    id("org.danilopianini.gradle-java-qa") version "0.37.0"
-}
-
-repositories {
-    mavenCentral()
-}
-
-application {
-    mainClass.set("it.unibo.collections.social.TestSocialNetworkUser")
 }
