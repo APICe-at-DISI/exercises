9ae5e6b3bc69e0503828a84c41c143fa13b3c6b1
Danilo Pianini <danilo.pianini@unibo.it>
2022-11-04 12:02:13 +0100

    begin preparing the exercise

--- java/gui/mvc-gui/src/main/java/it/unibo/mvc/LaunchApp.java
+++ java/gui/mvc-gui/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,12 +1,9 @@
 package it.unibo.mvc;
 
 import it.unibo.mvc.api.DrawNumberController;
-import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
+import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
@@ -19,27 +16,9 @@ public final class LaunchApp {
      * @param args
      *            ignored
      */
-    public static void main(final String... args)
-        throws
-        ClassNotFoundException,
-        NoSuchMethodException,
-        InvocationTargetException,
-        InstantiationException,
-        IllegalAccessException {
+    public static void main(final String... args) {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        for (final var viewType: List.of("StandardOutput", "Swing")) {
-            final var clazz = Class.forName("it.unibo.mvc.view.DrawNumber" + viewType + "View");
-            for (int i = 0; i < 3; i++) {
-                final var newView = clazz.getConstructor().newInstance();
-                if (DrawNumberView.class.isAssignableFrom(newView.getClass())) {
-                    app.addView((DrawNumberView) newView);
-                } else {
-                    throw new IllegalStateException(
-                        newView.getClass() + " is not a subclass of " + DrawNumberView.class
-                    );
-                }
-            }
-        }
+        app.addView(new DrawNumberSwingView());
     }
 }
--- java/gui/mvc-gui/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ java/gui/mvc-gui/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -3,10 +3,8 @@ package it.unibo.mvc.controller;
 import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
-import it.unibo.mvc.api.DrawResult;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 /**
  * This class implements the game controller. It orchestrates the game, exposes methods to its observers
@@ -15,7 +13,7 @@ import java.util.List;
 public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
-    private final List<DrawNumberView> views = new ArrayList<>(0);
+    private DrawNumberView view;
 
     /**
      * Builds a new game controller provided a game model.
@@ -28,17 +26,18 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
 
     @Override
     public void addView(final DrawNumberView view) {
-        views.add(view);
+        Objects.requireNonNull(view, "Cannot set a null view");
+        if (this.view != null) {
+            throw new IllegalStateException("The view is already set! Multiple views are not supported");
+        }
+        this.view = view;
         view.setController(this);
         view.start();
     }
 
     @Override
     public void newAttempt(final int n) {
-        final DrawResult result = model.attempt(n);
-        for (final DrawNumberView view: views) {
-            view.result(result);
-        }
+        Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));
     }
 
     @Override
