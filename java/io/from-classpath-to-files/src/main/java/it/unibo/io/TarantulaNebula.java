66b727bfa18cac17a72a4777e00bece1a578edbc
Danilo Pianini <danilo.pianini@unibo.it>
2022-11-24 17:05:57 +0100

    remove solution from the classpath read/filesystem write exercise

--- java/io/from-classpath-to-files/src/main/java/it/unibo/io/TarantulaNebula.java
+++ java/io/from-classpath-to-files/src/main/java/it/unibo/io/TarantulaNebula.java
@@ -20,29 +20,6 @@ public final class TarantulaNebula {
      * @param args one mandatory argument representing the destination path
      */
     public static void main(final String[] args) {
-        if (args.length != 1) {
-            System.out.println(// NOPMD
-                "This application requires exactly one parameter (the destination directory) to work, but "
-                    + args.length
-                    + " were provided instead"
-                    + (args.length == 0 ? "." : ": " + Arrays.asList(args))
-            );
-            System.exit(1);
-        }
-        final File destination = new File(args[0]);
-        if (!destination.exists() && !destination.mkdirs()) {
-            System.out.println("Cannot create folder " + destination.toPath()); // NOPMD
-            System.exit(2);
-        }
-        if (destination.isFile()) {
-            System.out.println(destination.toPath() + " is a file, not a directory!");  // NOPMD
-            System.exit(3);
-        }
-        for (final var resourceName: List.of("jwst-tarantula-nebula.jpg", "tarantula-nebula.md")) {
-            Exporter.exportResourceAs(
-                getSystemResource(RESOURCE_PATH + resourceName),
-                new File(destination + File.separator + resourceName)
-            );
-        }
+        // Implement here. Add any other classes as desired, if needed.
     }
 }
