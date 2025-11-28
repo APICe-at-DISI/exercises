f16ba496dd6ce1f6d7991bba53aa4c3bd2146c78
Danilo Pianini <danilo.pianini@unibo.it>
2025-11-28 14:13:28 +0100

    fix the configuration of es3

--- java/exams/es3/src/main/java/it/unibo/es3/Test.java
+++ java/exams/es3/src/main/java/it/unibo/es3/Test.java
@@ -12,7 +12,7 @@ public final class Test {
      * che realizza una semplice animazione, che effettua uno scatto ad ogni pressione del pulsante ">", mostrata nella
      * sequenza delle immagini fig1.png, fig2.png, fig3.png, fig4.png,...
      * 1 - all'inizio la griglia è vuota, con tre celle attive (con asterisco) scelte in modo random
-     * 2 - ad ogni pressione si attiva anche ogni cella che abbia una cella vicina già attiva
+     * 2 - a ogni pressione si attiva anche ogni cella che abbia una cella vicina già attiva
      *     (vicina orizzontalmente, verticalmente, o diagonalmente)
      * 3 - quando tutte le celle sono attive si esca
      *
