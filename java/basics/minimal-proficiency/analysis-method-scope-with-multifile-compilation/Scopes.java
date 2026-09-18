ace06c5f8d631f6295e6c6118b870528901c9fa9
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-09-18 14:31:56 +0200

    fix: change all system outputs into IO usage in java 25

--- java/basics/minimal-proficiency/analysis-method-scope-with-multifile-compilation/Scopes.java
+++ java/basics/minimal-proficiency/analysis-method-scope-with-multifile-compilation/Scopes.java
@@ -5,24 +5,24 @@ class Scopes {
     void build(int a, int b) {
         this.a = a;
         this.b = b;
-        System.out.print("[build method:] I can access the field a (" + this.a + "),");
-        System.out.print(" the field b (" + this.b + "),");
-        System.out.print(" the input parameter a (" + a + ")");
+        IO.println("[build method:] I can access the field a (" + this.a + "),");
+        IO.println(" the field b (" + this.b + "),");
+        IO.println(" the input parameter a (" + a + ")");
         IO.println(" and the input parameter b (" + b + ").");
     }
 
     void dummyMethod(int c) {
-        System.out.print("[dummyMethod:] I can access the field a (" + this.a + "),");
-        System.out.print(" the field b (" + this.b + ")");
+        IO.println("[dummyMethod:] I can access the field a (" + this.a + "),");
+        IO.println(" the field b (" + this.b + ")");
         IO.println(" and the input parameter c (" + c + ").");
     }
 
     void dummyMethod2(int a) {
         int b = 0;
-        System.out.print("[dummyMethod2:] I can access the field a (" + this.a + "),");
-        System.out.print(" the field b (" + this.b + "),");
-        System.out.print(" the input parameter a (" + a + "),");
-        System.out.print(" and the local variable b (" + b + ").");
+        IO.println("[dummyMethod2:] I can access the field a (" + this.a + "),");
+        IO.println(" the field b (" + this.b + "),");
+        IO.println(" the input parameter a (" + a + "),");
+        IO.println(" and the local variable b (" + b + ").");
         IO.println("I have no access to c, dummyMethod's input parameter.");
     }
 }
